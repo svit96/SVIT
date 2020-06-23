c0e2135698b8764de6a9372724ded82173f27286
ndthinh_it <thinhnuyenxc99@gmail.com>
2020-06-24 02:37:45 +0700

    using lombok

--- back-end/src/main/java/com/svtt/library/library/domain/Book.java
+++ back-end/src/main/java/com/svtt/library/library/domain/Book.java
@@ -1,10 +1,17 @@
 package com.svtt.library.library.domain;
 
 import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,45 +29,4 @@ public class Book {
     @Column(name = "img_url")
     @NotNull
     private String imgUrl;
-
-    public Book() {
-    }
-
-    public Book(String name, String author, String imgUrl) {
-        this.name = name;
-        this.author = author;
-        this.imgUrl = imgUrl;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
 }
